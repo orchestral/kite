47ed3e281e78b1d66a555bb04698ef8a42818ec4
crynobone <crynobone@gmail.com>
2016-06-15 21:20:34 +0800

    Update register page.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/packages/orchestra/foundation/credential/register.blade.php
+++ resources/views/packages/orchestra/foundation/credential/register.blade.php
@@ -1,9 +1,17 @@
 @extends('orchestra/foundation::layouts.sign')
 
 @section('content')
-<div class="row">
-	<div class="six columns offset-by-three">
-		{!! $form !!}
-	</div>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h4 class="panel-title">{{ trans('orchestra/foundation::title.register') }}</h4>
+  </div>
+
+  <div class="panel-body">
+    {{ $form }}
+  </div>
+
+  <div class="panel-footer">
+    <a href="{{ handles('orchestra::login') }}"><i class="fa fa-arrow-left"></i> {{ trans('orchestra/foundation::title.login') }}</a>
+  </div>
 </div>
 @stop
