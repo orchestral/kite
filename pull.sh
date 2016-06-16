09e988a8502fbebe9d8b487ac2ed10f3944f5b17
crynobone <crynobone@gmail.com>
2016-06-16 11:12:45 +0800

    Working on pulling latest code.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- pull.sh
+++ pull.sh
@@ -18,3 +18,15 @@ if [[ -d ../foundation ]]; then
     cp -Rf ../foundation/resources/js/* ./resources/js/
     cp -Rf ../foundation/resources/views/* ./resources/views/packages/orchestra/foundation/
 fi
+
+if [[ -d ../story ]]; then
+    echo '>>> Sync Foundation'
+
+    cd ../story
+    git checkout $BRANCH
+    cd $DIR
+
+    rm -Rf ./resources/views/packages/orchestra/story/*
+
+    cp -Rf ../story/resources/views/* ./resources/views/packages/orchestra/story/
+fi
