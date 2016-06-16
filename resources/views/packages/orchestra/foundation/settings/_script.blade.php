4fc159db08c2486a794a0d4fc7dcad1920ca9fc8
crynobone <crynobone@gmail.com>
2016-06-16 08:02:40 +0800

    Refactor global objects.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/views/packages/orchestra/foundation/settings/_script.blade.php
+++ resources/views/packages/orchestra/foundation/settings/_script.blade.php
@@ -1,7 +1,5 @@
 <script>
 jQuery(function($) {
-  var driver
-
   Javie.on('setting.changed: email.driver.smtp', function (app) {
     $.each(['email_host', 'email_port', 'email_username', 'email_password', 'email_encryption'], function(index, name) {
       app.container('input[name="'+name+'"]').removeClass('hidden')
