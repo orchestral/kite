d7709bc5098f900f2649f9c966fb5134d39d28aa
crynobone <crynobone@gmail.com>
2016-07-12 22:23:37 +0800

    Move to elixir 6 and webpack.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- paths.js
+++ paths.js
@@ -1,19 +1,19 @@
 module.exports = {
   asset: {
-    css: './public/packages/orchestra/foundation/css',
-    font: './public/packages/orchestra/foundation/fonts',
-    img: './public/packages/orchestra/foundation/img',
-    js: './public/packages/orchestra/foundation/js'
+    css: 'public/packages/orchestra/foundation/css',
+    font: 'public/packages/orchestra/foundation/fonts',
+    img: 'public/packages/orchestra/foundation/img',
+    js: 'public/packages/orchestra/foundation/js'
   },
   build: {
-    css: './resources/assets/css',
-    font: './resources/assets/fonts',
-    js: './resources/assets/js',
-    less: './resources/assets/less',
-    img: './resources/assets/img',
-    vendor: './resources/assets/vendor'
+    css: 'resources/assets/css',
+    font: 'resources/assets/fonts',
+    js: 'resources/assets/js',
+    less: 'resources/assets/less',
+    img: 'resources/assets/img',
+    vendor: 'resources/assets/vendor'
   },
-  js: './resources/js',
-  html: './resources/html',
-  vendor: './vendor/bower_components'
+  js: 'resources/js',
+  html: 'resources/html',
+  vendor: 'vendor/bower_components'
 }
